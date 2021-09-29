70c9312ccd55594f95333916fd4c15aebb0fc8f5
Dimple <dimplem@shriffle.com>
2021-09-29 19:16:23 +0530

    learning js

---
+++ index.js
@@ -0,0 +1,111 @@
+
+// var $myname='dimple';
+// console.log($myname);
+// Naming Practice
+// var _myName = "dimple";
+//  var 1myName = "malviya"; //not valid
+// var _1my__Name = "dimpi";
+// var $myName = "dimple malviya";
+// var myNaem% = "dimple malviya";
+// console.log( $myName);
+// console.log(10+20+"30");
+// console.log(10-"5");bug
+//console.log('hello' - 'bye');bug
+// var myName='dimple';
+// var myNumber=6587495323;
+// console.log(isNaN(myNumber));
+// if(isNaN(myNumber)){
+//     myNumber=654823563;
+//     console.log(myNumber);
+// }
+// else{
+//     console.log(myName);
+// }
+//nan practice
+//variables,data-types
+// console.log(NaN===NaN);
+// console.log(Number.NaN===NaN);
+// console.log(isNaN(NaN));
+// console.log(isNaN(Number.NaN));
+// console.log(Number.isNaN(NaN));
+//expressions and  operators
+//combination of operators and operand is expression
+// var x=5, y=5;
+// console.log("check equality" + (x==y));
+// var num=5;
+// console.log(num);
+// var number=--num -5;
+// console.log(number);
+// var demo=["hello", "hiii"];
+// console.log(demo[0]);
+// var a=20, b=30, c=20;
+// console.log(!(a>b || a>c));
+// var Name='dimple';
+// console.log(Name +" demo");
+// console.log(Name +" demo12");
+// console.log(4**4); exponentiation(first operand treated as power of second operand)
+//  var a=10, b=20; //swaping variable using third variable
+//  var c=b;
+//  b=a;
+//  a=c;
+//  console.log(a, b)
+//without hird variable
+// var a=10, b=20;
+// a=a+b ; //30
+// b=a-b ;  //10
+// a=a-b;
+// console.log("value of a " + a);
+// console.log("value of b " + b);
+// var num1="5", num2=5;
+// console.log(num1===num2);
+//diffrence between == and ===
+// var year=1900;
+// if(year%4==0){
+//     if(year%100==0){
+//         console.log('its leap year');
+//     }
+//     else{
+//         console.log('this year is not leap year');
+//     }
+// }
+// else {
+// console.log('its not leap year');
+// }
+//falsy values in javascript 0, "", undefined, NaN,null
+// if(null){
+// console.log('this is false')
+// }
+// else{
+//     console.log('this is not')
+// }
+//ternary operator is the only one that takes three operands
+// variablename=(condition)? value1:value2;
+// var age=10;
+// console.log((age>18)? 'eligible':'not eligible');
+// var num1=20;
+// console.log(num1);
+// var num2=30;
+
+// let num2=30;
+// let sum=myfunc();
+// function myfunc(){
+//     let num2=40;
+//     console.log(num2);
+// }
+// console.log(num2);
+// let a=2;
+// let an=2;
+// console.log(a,an);
+
+//var, let and const
+ //declare variable using var can acces before declaration but while using let and const cant access
+ //use const  unless you know that the value will change. we can use this to declare array,object,function
+//const must initialise with value
+ // myName='dimple';
+// var myName;
+// console.log(myName);
+// myname2='dimple';
+// let myname2;
+// console.log(myname2);
+
+
